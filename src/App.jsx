92b135fe41fbd5f865256961f553ab13b969a88f
MiguelAstudilloCampo <miguelangelastudillo38@gmail.com>
2024-06-11 17:17:48 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+import { BrowserRouter, useRoutes } from "react-router-dom";
+
+// Navs
+
+
+
+/// Paginas Principales
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+/// Rol Vigilante
+
+
+/// Rol Administrativo
+
+/// Rol Instructor
+
+/// Rol Aprendiz
+
+function App() {
+
+  const AppRoutes = () => {
+    let routes = useRoutes([
+      
+      // Rutas Principales
+
+      // { path: "/", element: <Home/> },
+      { path: "/", element: <Login/> },
+      { path: "/Register", element: <Register/> },
+
+      // Rutas Vigilante
+
+      // Rutas Administrativo
+
+      // Rutas Instructor
+
+      // Rutas Aprendiz
+
+    ]);
+    return routes;
+  };
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
